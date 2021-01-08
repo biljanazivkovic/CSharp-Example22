a57b1e0f4349a3d59c25de1c5e1a56884cdcb54f
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:43:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ex22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "Hello World!";
+            File.WriteAllText("text.txt", writeText);
+
+            string readText = File.ReadAllText("text.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
